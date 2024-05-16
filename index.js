87825c3ade50bfa76ce074c150b6e6392974d5d2
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-05-16 14:55:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
 })
 
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-
 app.use('/',async (req, res, next) => {
 res.sendFile(__path + '/main.html')
 })
