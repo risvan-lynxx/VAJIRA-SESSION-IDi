4e933ee6d122dc65f66d3979a7637fb622345310
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-07-16 10:15:10 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -72,6 +72,17 @@ var randomItem = selectRandomItem(items);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
                         let md = "VAJIRA-MD=" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
+                      //  const id = m.quoted.id;
+const buttons = [
+                {
+                    "name": "cta_copy",
+                    "buttonParamsJson": JSON.stringify({
+                        "display_text": "Copy Id",
+                        "id": "copy_email",
+                        "copy_code": md
+                    })
+                }
+            ];		
                         let desc = `*𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! 𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚝𝚘 𝚌𝚛𝚎𝚊𝚝𝚎 𝚅𝙰𝙹𝙸𝚁𝙰-𝙼𝙳 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝚋𝚘𝚝.*\n\n ◦ *Github:* https://github.com/VajiraTech/VAJIRA-MD`;
                         await sock.sendMessage(sock.user.id, {
 text: desc,
