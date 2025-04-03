cbc853b71a834cfd9f3f3ac5111d824a3c7aa4c1
lLynx <risvanlynx777@gmail.com>
2025-04-03 14:49:29 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -78,7 +78,7 @@ text: desc,
 
 
 						       
-{quoted:code })
+quoted:code })
                     } catch (e) {
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
                             let desc = `*𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! 𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚝𝚘 𝚌𝚛𝚎𝚊𝚝𝚎 𝚅𝙰𝙹𝙸𝚁𝙰-𝙼𝙳 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝚋𝚘𝚝.*\n\n ◦ *Github:* https://github.com/VajiraTech/VAJIRA-MD`;
@@ -87,7 +87,7 @@ text: desc,
 
 
 			    					   
-{quoted:ddd })
+quoted:ddd })
                     }
                     await delay(10);
                     await sock.ws.close();
